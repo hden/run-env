024fe4ada3ee94ddec70f0049126d43cc3c7fd8a
Haokang Den <haokang.den@gmail.com>
2019-10-22 18:28:26 +0800

    Update delete_test.clj

--- test/run_env/formats/delete_test.clj
+++ test/run_env/formats/delete_test.clj
@@ -5,11 +5,11 @@
 (deftest format-parameters
   (testing "configurations"
     (are [expected m] (= expected (delete/format-parameters m))
-      "gcloud beta run services delete service"
+      "yes | gcloud beta run services delete service"
       {:name "service"
        :image "gcr.io/cloudrun/hello"}
 
-      "gcloud beta run services delete service --region=asia-northeast1"
+      "yes | gcloud beta run services delete service --region=asia-northeast1"
       {:name "service"
        :image "gcr.io/cloudrun/hello"
        :concurrency 10
